b87f695fbe03192a8e6e306b63b063542008d231
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-07-22 14:32:28 -0400

    Return number of attempts from function

--- functions/start-verify.js
+++ functions/start-verify.js
@@ -22,7 +22,7 @@
 // eslint-disable-next-line consistent-return
 exports.handler = function (context, event, callback) {
   const response = new Twilio.Response();
-  response.appendHeader('Content-Type', 'application/json');
+  response.appendHeader("Content-Type", "application/json");
 
   /*
    * uncomment to support CORS
@@ -31,12 +31,12 @@ exports.handler = function (context, event, callback) {
    * response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
    */
 
-  if (typeof event.to === 'undefined') {
+  if (typeof event.to === "undefined") {
     response.setBody({
       success: false,
       error: {
-        message: 'Missing parameter; please provide a phone number or email.',
-        moreInfo: 'https://www.twilio.com/docs/verify/api/verification',
+        message: "Missing parameter; please provide a phone number or email.",
+        moreInfo: "https://www.twilio.com/docs/verify/api/verification",
       },
     });
     response.setStatusCode(400);
@@ -48,13 +48,13 @@ exports.handler = function (context, event, callback) {
    * Learn more about toll fraud
    * https://www.twilio.com/docs/verify/preventing-toll-fraud
    */
-  if (event.channel === 'call') {
+  if (event.channel === "call") {
     response.setBody({
       success: false,
       error: {
         message:
-          'Calls disabled by default. Update the code in <code>start-verify.js</code> to enable.',
-        moreInfo: 'https://www.twilio.com/docs/verify/preventing-toll-fraud',
+          "Calls disabled by default. Update the code in <code>start-verify.js</code> to enable.",
+        moreInfo: "https://www.twilio.com/docs/verify/preventing-toll-fraud",
       },
     });
     response.setStatusCode(400);
@@ -64,8 +64,8 @@ exports.handler = function (context, event, callback) {
   const client = context.getTwilioClient();
   const service = context.VERIFY_SERVICE_SID;
   const { to } = event;
-  const channel = typeof event.channel === 'undefined' ? 'sms' : event.channel;
-  const locale = typeof event.locale === 'undefined' ? 'en' : event.locale;
+  const channel = typeof event.channel === "undefined" ? "sms" : event.channel;
+  const locale = typeof event.locale === "undefined" ? "en" : event.locale;
 
   client.verify
     .services(service)
@@ -79,6 +79,7 @@ exports.handler = function (context, event, callback) {
       response.setStatusCode(200);
       response.setBody({
         success: true,
+        attempts: verification.sendCodeAttempts.length,
       });
       return callback(null, response);
     })
